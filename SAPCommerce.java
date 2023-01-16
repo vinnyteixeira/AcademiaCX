87c26ce5c6059293d2a28af6ad44f8049bb5cf8a
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2023-01-16 11:14:36 -0300

    Atividade 01 academia cx

---
+++ SAPCommerce.java
@@ -0,0 +1,8 @@
+public class SAPCommerce {
+    public void AddToCart() {
+        System.out.println("Adicionando item ao carrinho"); 
+    }
+     public void removeItemFromCart() {
+        System.out.println("Removendo item ao carrinho"); 
+    }
+}
---
+++ TestaSAPCommerce.java
@@ -0,0 +1,8 @@
+public class TestaSAPCommerce {
+	public static void main (String [] args){
+	SAPCommerce sapcommerce = new SAPCommerce();
+
+	sapcommerce.AddToCart();
+	sapcommerce.removeItemFromCart();
+	}
+}
